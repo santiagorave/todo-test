fa671a5ec5e2bef26948b9ce45302658e049866e
Xavi <santiagorave98@gmail.com>
2024-10-08 13:53:51 -0500

    SCSS and some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <main>
       <h1>To Do List</h1>
-    </>
-  )
+      <section className="bg">
+        <section className="list">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </section>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
